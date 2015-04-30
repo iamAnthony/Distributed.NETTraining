554002561b1c9dd107b8f1f9cbc8614c8dbce76b
Anthony Aubrée <anthony.aubree@etu.univ-lyon1.fr>
2015-04-30 11:51:43 +0200

    Reste à implémenter les tests repository

--- 2-CQRSTwitterLike/Messaging.Tests/Domain/TimelineMessageTests.cs
+++ 2-CQRSTwitterLike/Messaging.Tests/Domain/TimelineMessageTests.cs
@@ -8,13 +8,29 @@ namespace Messaging.Tests.Domain
     [TestFixture]
     public class TimelineMessageTests
     {
+
+        private UserId uid;
+
+        public UserId Uid
+        {
+            get { return uid; }
+        }
+
+        [SetUp]
+        public void newUser()
+        {
+            uid = new UserId();
+        }
+
         [Test]
         public void WhenInstanciateTimelineMessage_ThenCanGetEachProperties()
         {
-            var timelineMessage = new TimelineMessage("Jordan","Jordan",DateTime.Today, 0, 0, "Hello World!");
 
-            Check.That(timelineMessage.Owner).IsEqualTo("Jordan");
-            Check.That(timelineMessage.Author).IsEqualTo("Jordan");
+            var timelineMessage = new TimelineMessage(0,Uid,Uid,DateTime.Today, 0, 0, "Hello World!");
+            
+            Check.That(timelineMessage.Id).IsEqualTo(0);
+            Check.That(timelineMessage.Owner).IsEqualTo(Uid);
+            Check.That(timelineMessage.Author).IsEqualTo(Uid);
             Check.That(timelineMessage.Date).IsEqualTo(DateTime.Today);
             Check.That(timelineMessage.NbFav).IsEqualTo(0);
             Check.That(timelineMessage.NbRt).IsEqualTo(0);
@@ -24,8 +40,8 @@ namespace Messaging.Tests.Domain
         [Test]
         public void WhenInstanciateTwoTimelineMessageWithSameProperties_ThenTheyAreEquals()
         {
-            var timelineMessage1 = new TimelineMessage("Jordan", "Jordan", DateTime.Today, 0, 0, "Hello World!");
-            var timelineMessage2 = new TimelineMessage("Jordan", "Jordan", DateTime.Today, 0, 0, "Hello World!");
+            var timelineMessage1 = new TimelineMessage(0,Uid,Uid, DateTime.Today, 0, 0, "Hello World!");
+            var timelineMessage2 = new TimelineMessage(0,Uid, Uid, DateTime.Today, 0, 0, "Hello World!");
 
             Check.That(timelineMessage1).IsEqualTo(timelineMessage2);
         }
---
+++ 2-CQRSTwitterLike/Messaging/Infrastructure/TimelineMessageRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Messaging.Infrastructure
+{
+    public class TimelineMessageRepository
+    {
+        private IEnumerable<TimelineMessage> myAttribute;
+        public TimelineMessageRepository(IEnumerable<TimelineMessage> getFakeTimelineMessages)
+        {
+            myAttribute = getFakeTimelineMessages;
+        }
+
+        public IEnumerable<TimelineMessage> GetLastMessagesForUser(UserId userId, int i)
+        {
+            // TODO
+        }
+    }
+}
