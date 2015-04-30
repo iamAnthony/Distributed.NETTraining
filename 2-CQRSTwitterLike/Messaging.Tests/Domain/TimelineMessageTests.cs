0c9d7f444e1caa7197539fe2c593c79e1f464959
Anthony Aubrée <anthony.aubree@etu.univ-lyon1.fr>
2015-04-30 10:50:08 +0200

    TimelineMessageTests : OK

--- 2-CQRSTwitterLike/Messaging.Tests/Domain/TimelineMessageTests.cs
+++ 2-CQRSTwitterLike/Messaging.Tests/Domain/TimelineMessageTests.cs
@@ -1,4 +1,6 @@
-﻿using NFluent;
+﻿using System;
+using Messaging.Infrastructure;
+using NFluent;
 using NUnit.Framework;
 
 namespace Messaging.Tests.Domain
@@ -6,22 +8,26 @@ namespace Messaging.Tests.Domain
     [TestFixture]
     public class TimelineMessageTests
     {
-        //[Test]
-        //public void WhenInstanciateTimelineMessage_ThenCanGetEachProperties()
-        //{
-        //    var timelineMessage = new TimelineMessage(/* TODO add required properties in constructor parameters */);
+        [Test]
+        public void WhenInstanciateTimelineMessage_ThenCanGetEachProperties()
+        {
+            var timelineMessage = new TimelineMessage("Jordan","Jordan",DateTime.Today, 0, 0, "Hello World!");
 
-        //    // TODO : add a check for each properties getter (NO SETTER ! => )
-        //    Check.That(timelineMessage./* TODO [property] */).IsEqualTo(/* TODO */)
-        //}
+            Check.That(timelineMessage.Owner).IsEqualTo("Jordan");
+            Check.That(timelineMessage.Author).IsEqualTo("Jordan");
+            Check.That(timelineMessage.Date).IsEqualTo(DateTime.Today);
+            Check.That(timelineMessage.NbFav).IsEqualTo(0);
+            Check.That(timelineMessage.NbRt).IsEqualTo(0);
+            Check.That(timelineMessage.Content).IsEqualTo("Hello World!");
+        }
 
-        //[Test]
-        //public void WhenInstanciateTwoTimelineMessageWithSameProperties_ThenTheyAreEquals()
-        //{
-        //    var timelineMessage1 = new TimelineMessage(/* TODO add required properties in constructor parameters */);
-        //    var timelineMessage2 = new TimelineMessage(/* TODO add required properties in constructor parameters */);
+        [Test]
+        public void WhenInstanciateTwoTimelineMessageWithSameProperties_ThenTheyAreEquals()
+        {
+            var timelineMessage1 = new TimelineMessage("Jordan", "Jordan", DateTime.Today, 0, 0, "Hello World!");
+            var timelineMessage2 = new TimelineMessage("Jordan", "Jordan", DateTime.Today, 0, 0, "Hello World!");
 
-        //    Check.That(timelineMessage1).IsEqualTo(timelineMessage2);
-        //}
+            Check.That(timelineMessage1).IsEqualTo(timelineMessage2);
+        }
     }
 }
--- 2-CQRSTwitterLike/Messaging/Infrastructure/MessagingModule.cs
+++ 2-CQRSTwitterLike/Messaging/Infrastructure/MessagingModule.cs
@@ -3,6 +3,57 @@ using Nancy;
 
 namespace Messaging.Infrastructure
 {
+    public struct TimelineMessage
+    {
+        private readonly string owner;
+        private readonly string author;
+        private readonly DateTime date;
+        private readonly int nbFav;
+        private readonly int nbRT;
+        private readonly string content;
+
+        public TimelineMessage(string owner, string author, DateTime date, int nbFav, int nbRt, string content)
+        {
+            
+            this.owner = owner;
+            this.author = author;
+            this.date = date;
+            this.nbFav = nbFav;
+            this.nbRT = nbRt;
+            this.content = content;
+        }
+
+        public string Owner
+        {
+            get { return owner; }
+        }
+
+        public string Author
+        {
+            get { return author; }
+        }
+
+        public DateTime Date
+        {
+            get { return date; }
+        }
+
+        public int NbFav
+        {
+            get { return nbFav; }
+        }
+
+        public int NbRt
+        {
+            get { return nbRT; }
+        }
+
+        public string Content
+        {
+            get { return content; }
+        }
+    }
+
     public class MessagingModule : NancyModule
     {
         public MessagingModule()
